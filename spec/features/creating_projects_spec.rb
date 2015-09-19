06543ff51a354f7168298114cbb6b33a666b6027
Zach Toolson <zach.toolson@gmail.com>
2015-09-19 11:09:21 -0600

    Add title functionality for projects show page

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -2,14 +2,22 @@ require "spec_helper"
 
 feature "Creating Projects" do
   scenario "can create a project" do
+    project_name = "TextMate 2"
+
     visit "/"
 
     click_link "New Project"
 
-    fill_in "Name", with: "TextMate 2"
+    fill_in "Name", with: project_name
     fill_in "Description", with: "A text-editor for OS X"
     click_button "Create Project"
 
     expect(page).to have_content("Project was successfully created")
+
+    project = Project.where(name: project_name).first
+    expect(page.current_url).to eq(project_url(project))
+
+    title = "#{project_name} - Projects - Ticketee"
+    expect(page).to have_title(title)
   end
 end
